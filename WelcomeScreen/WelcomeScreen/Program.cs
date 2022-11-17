85912ab36a4305070f46e9bac9da25ca27b8a6c6
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-11-17 18:19:00 -0500

    made changes to WelcomeScreen

---
+++ WelcomeScreen/WelcomeScreen/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WelcomeScreen
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting\nRemember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
