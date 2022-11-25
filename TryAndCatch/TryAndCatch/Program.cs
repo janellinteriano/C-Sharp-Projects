6e3beb9bd3a6e231ddae315e12c71ed79fbe1422
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-11-25 01:37:00 -0500

    added changes to TryAndCatch

---
+++ TryAndCatch/TryAndCatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryAndCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating a list of integers
+            var numberList = new List<int> { 12, 16, 20, 24, 36 };
+
+            // try and catch block with foreach loop
+            try
+            {
+                // saving user input as int
+                Console.WriteLine("Enter a number you want to use to divide:");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                foreach (var i in numberList)
+                {
+                    var answer = i / userNumber; 
+                    Console.WriteLine(i + " divided by " + userNumber + " equals " + answer);
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please do not divide by zero.");
+            }
+            catch (SystemException)
+            {
+                Console.WriteLine("Please only enter a number.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
