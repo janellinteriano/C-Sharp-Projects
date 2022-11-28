35b1784a0610bc64744b41089bae462225dd7761
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-11-28 18:56:43 -0500

    added changes to CallingMethods

---
+++ CallingMethods/CallingMethods/Math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    // created class Math
+    public class Math
+    {
+        
+        // created 3 methods Add, Subtract, and Multiply
+        // takes in parameters and returns integer
+        public static int Add(int number1, int number2)
+        {
+            int total;
+            total = number1 + number2;
+            return total;
+        } 
+        
+        public static int  Subtract(int number1, int number2)
+        {
+            int difference;
+            difference= number2 - number1;
+            return difference;
+        }
+
+        public static int Multiply(int number1, int number2)
+        {
+            int product;
+            product = number1 * number2;
+            return product;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // makes variables known
+            int a, b;
+            int c;
+            int d;
+            int e;
+
+            // saves user input as "a"
+            Console.WriteLine("Hello! Pick a number to do some math operations with:");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = 100;
+            c = Math.Add(a, b);
+            d = Math.Subtract(a, b);
+            e = Math.Multiply(a, b);
+            
+            // calls methods from Math class and displays answers to the console
+            Console.WriteLine("100 plus " + a + " equals " + c);
+            Console.WriteLine("100 minus " + a + " equals " + d);
+            Console.WriteLine("100 times " + a + " equals " + e);
+            Console.ReadLine();
+
+        }
+    }
+}
