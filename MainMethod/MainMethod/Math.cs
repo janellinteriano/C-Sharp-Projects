5f2f7ba971a5a7afdd5fca87ff22c982ec1f2da3
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-11-28 20:20:15 -0500

    added changes to MainMethod

---
+++ MainMethod/MainMethod/Math.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    // created class "Math"
+    class Math
+    {
+        // created 3 methods with same name "Operation"
+
+        // takes user input as int and adds 5
+        public static int Operation(int num1)
+        {
+            int answer;
+            answer = num1 + 5;
+            return answer;
+        }   
+        
+        // takes user decimal and times by five
+        // then returns as integer
+        public static int Operation(decimal num2)
+        {
+            decimal answer;
+            answer = num2 * 5;
+            return Convert.ToInt32(answer);
+        } 
+        
+        // takes user string and converts to integer
+        // subtracts 1 from integer
+        // returns answer as an integer
+        public static int Operation(string num3)
+        {
+            int answer;
+            answer = Convert.ToInt32(num3) - 1;
+            return Convert.ToInt32(answer);
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // making variables known
+            int num1;
+            decimal num2;
+            string num3;
+            int answer;
+
+            // saves user input
+            // instantiates class and calls first method
+            Math obj = new Math();
+            Console.WriteLine("Pick a whole number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            answer = Math.Operation(num1);
+
+            // displays answer to the console
+            Console.WriteLine(num1 + " plus 5 equals " + answer);
+            Console.ReadLine();
+
+            // saves user input
+            // instantiates class and calls second method
+            Math obj1 = new Math();
+            Console.WriteLine("Pick a decimal number:");
+            num2 = Convert.ToDecimal(Console.ReadLine());
+            answer = Math.Operation(num2);
+
+            // displays answer to the console
+            Console.WriteLine(num2 + " times 5 equals " + answer);
+            Console.ReadLine();
+
+            // saves user input
+            // instantiates class and calls third method
+            Math obj2 = new Math();
+            Console.WriteLine("Type in a whole number again:");
+            num3 = Console.ReadLine();
+            answer = Math.Operation(num3);
+
+            // displays answer to the console
+            Console.WriteLine(num3 + " minus 1 equals " + answer);
+            Console.ReadLine();
+        }    
+    }
+}
