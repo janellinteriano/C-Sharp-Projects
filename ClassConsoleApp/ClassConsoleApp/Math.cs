5abe116f004cb73df7a892b3fe9c663dccd1d1a4
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-11-29 02:59:52 -0500

    added changes to ClassConsoleApp

---
+++ ClassConsoleApp/ClassConsoleApp/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassConsoleApp
+{
+    // created class
+    class Math
+    {
+        // void method with one parameter
+        public void Operation(int number1)
+        {
+            // divides by 2 and displays to console
+            int division = number1 / 2;
+            Console.WriteLine(division);
+        }  
+        
+        // declaring static method
+        // overloading method
+        // has out parameters
+        public static void Operation(int number1, int number2, out int addition, out int subtraction)
+        {
+            addition = number1 + number2;
+            subtraction = number1 - number2;
+        }
+    }
+}
---
+++ ClassConsoleApp/ClassConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring variables 
+            int AdditionResult, SubtractionResult;
+            int number2 = 2; 
+            
+            // instantiates class
+            Math math = new Math();
+
+            // saves user input as number1
+            Console.WriteLine("Please enter a whole number:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            // calls first void method
+            math.Operation(number1);
+            Console.ReadLine();
+
+            // calls second method with out parameters
+            // displays to console
+            Math.Operation(number1, number2, out AdditionResult, out SubtractionResult);
+            Console.WriteLine($"{number1} + {number2} equals {AdditionResult}. {number1} - {number2} equals {SubtractionResult}.");
+            Console.ReadLine();
+        }
+    }
+}
