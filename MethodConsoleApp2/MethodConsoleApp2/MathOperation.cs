a6e6ba8223be1e6299c1d76135e34251e16206d9
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-11-28 23:15:14 -0500

    added changes to MethodConsoleApp2

---
+++ MethodConsoleApp2/MethodConsoleApp2/MathOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodConsoleApp2
+{
+    class MathOperation
+    {
+        public void Addition(int num1, int num2)
+        {
+            // adds 50 to num1
+            int num3 = num1 + 50;
+            // displays both numbers to console
+            Console.WriteLine(num2);
+            Console.ReadLine();
+            Console.WriteLine(num3);
+        }
+    }
+}
---
+++ MethodConsoleApp2/MethodConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates class                          
+            MathOperation operation = new MathOperation();
+            // calling parameters by name
+            // passes in two numbers
+            operation.Addition(num1: 9, num2: 10);
+            Console.ReadLine();
+        }
+    }
+}
