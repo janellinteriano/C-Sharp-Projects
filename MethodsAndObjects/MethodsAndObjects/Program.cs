e3f29236da4ca8addc38ed7051c17545d3f8ff20
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-11-30 21:02:40 -0500

    added changes to MethodsAndObjects

---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates employee object
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            // calling superclass method
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
