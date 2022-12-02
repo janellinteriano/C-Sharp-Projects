1ebc4b7c849d906764e0751b03e76c193bf517a2
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-12-01 23:48:48 -0500

    added changes to ParsingEnums

---
+++ ParsingEnums/ParsingEnums/Days.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    // created class
+    public class Days
+    {
+        public Days day { get; set; }
+        public string userDay { get; set; }
+            
+    } 
+
+    // created Enum for days of the week
+    public enum Day
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+
+    
+
+}
---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try and catch block
+            try
+            {
+                // instantiated object
+                Days day = new Days();
+                Console.WriteLine("What day of the week is it?");
+                // saves user input
+                day.userDay = Console.ReadLine().ToString();
+
+                // parses through Enum
+                Day day1;
+                bool checkParse = Enum.TryParse(day.userDay, out day1);
+
+                if (checkParse == true)
+                {
+                    Console.WriteLine("Correct! That is the current day of the week.");
+                }
+                if (checkParse != true)
+                {
+                    Console.WriteLine("That is incorrect. Try again.");
+                }
+            }   
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
