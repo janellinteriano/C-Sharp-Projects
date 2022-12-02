e6f919e55eb245100a04e7b1ff09aff7d57d448a
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-12-01 19:09:49 -0500

    added changes to Operators

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    // created employee class
+    public class Employee : IEquatable<Employee>
+    {
+        // set properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+
+        // == operator
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+
+            if (employee1.Id.Equals(employee2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // != operator
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (!employee1.Id.Equals(employee2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Equals (Employee other)
+        {
+            return other.Id.Equals(this.Id);
+        } 
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates first employee
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Anna";
+            employee1.LastName = "Summer";
+            employee1.Id = 0014;
+
+            // instantiates second employee
+            Employee employee2 = new Employee();
+            employee2.FirstName = "Branden";
+            employee2.LastName = "Anderson";
+            employee2.Id = 0026;
+
+            // if statement to display either message
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("employee1 is equal to employee2");
+            }
+            else
+            {
+                Console.WriteLine("employee1 is not equal to employee2");
+            }
+            Console.ReadLine();
+        }
+    }
+}
