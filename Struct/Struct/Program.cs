6b6712b28b8ab67985bfa0105cdb0a5c549f14b6
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-12-02 00:11:16 -0500

    added changes to Struct

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates object
+            Number number = new Number();
+            // converts value to decimal form
+            number.Amount = Convert.ToDecimal(12.75);
+
+            //displays to console
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
