3a609f3499b2873f947a2d9607b5a918dd976584
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-12-04 16:37:33 -0500

    added changes to InputApp

---
+++ InputApp/InputApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number:");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\janel\mylog.txt"))
+            {
+                file.WriteLine(answer);
+            }
+        }
+    }
+}
