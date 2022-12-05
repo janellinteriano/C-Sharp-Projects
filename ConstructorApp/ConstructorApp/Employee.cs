27b9f240bb66d2249066e6d22f7308e51da92bca
janellinteriano <111891542+janellinteriano@users.noreply.github.com>
2022-12-04 20:04:19 -0500

    added changes to ConstructorApp

---
+++ ConstructorApp/ConstructorApp/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorApp
+{
+    public class Employee
+    {
+        // setting properties
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int Id { get; set; }
+
+        // chaining constructors and giving last name "Garcia" as default
+        public Employee(string firstName, int Id) : this(firstName, "Garcia", Id)
+        {
+        }   
+        
+        public Employee(string firstName, string lastName, int Id)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.Id = Id;
+        }
+    }
+}
---
+++ ConstructorApp/ConstructorApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiating object
+            Employee employee = new Employee("Janell", 1226);
+            // setting constant string
+            const string greeting = "Hello! welcome to your first day on the job ";
+            Console.WriteLine($"{greeting}{employee.firstName}!");
+            Console.ReadLine();
+            Console.WriteLine("What is your favorite number?");
+            // saves user input as int
+            var favoriteNumber = Convert.ToInt32(Console.ReadLine());
+            // displays message to console
+            Console.WriteLine($"Okay. Your employee Id number is {employee.Id} and your favorite color is {favoriteNumber}!");
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
